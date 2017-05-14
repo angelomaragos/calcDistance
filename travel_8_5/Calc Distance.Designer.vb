68037248216f082415a3c78825574af48a65e83c
Angelo Maragos <angelomaragos@hotmail.com>
2017-05-13 23:23:40 -0400

    first commit

---
+++ travel_8_5/Calc Distance.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class calcForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblDepart = New System.Windows.Forms.Label()
+        Me.lblDest = New System.Windows.Forms.Label()
+        Me.ComboBoxDepart = New System.Windows.Forms.ComboBox()
+        Me.ComboBoxDest = New System.Windows.Forms.ComboBox()
+        Me.btnLookUp = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        Me.lblDistance = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblDepart
+        '
+        Me.lblDepart.AutoSize = True
+        Me.lblDepart.Location = New System.Drawing.Point(26, 36)
+        Me.lblDepart.Name = "lblDepart"
+        Me.lblDepart.Size = New System.Drawing.Size(54, 13)
+        Me.lblDepart.TabIndex = 0
+        Me.lblDepart.Text = "Departure"
+        '
+        'lblDest
+        '
+        Me.lblDest.AutoSize = True
+        Me.lblDest.Location = New System.Drawing.Point(212, 36)
+        Me.lblDest.Name = "lblDest"
+        Me.lblDest.Size = New System.Drawing.Size(60, 13)
+        Me.lblDest.TabIndex = 1
+        Me.lblDest.Text = "Destination"
+        '
+        'ComboBoxDepart
+        '
+        Me.ComboBoxDepart.FormattingEnabled = True
+        Me.ComboBoxDepart.Items.AddRange(New Object() {"Boston", "Chicago", "Dallas", "Las Vegas", "Los Angeles", "Miami", "New Orleans", "Toronto", "Vancouver", "Washington DC"})
+        Me.ComboBoxDepart.Location = New System.Drawing.Point(29, 68)
+        Me.ComboBoxDepart.Name = "ComboBoxDepart"
+        Me.ComboBoxDepart.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBoxDepart.TabIndex = 2
+        '
+        'ComboBoxDest
+        '
+        Me.ComboBoxDest.FormattingEnabled = True
+        Me.ComboBoxDest.Items.AddRange(New Object() {"Boston", "Chicago", "Dallas", "Las Vegas", "Los Angeles", "Miami", "New Orleans", "Toronto", "Vancouver", "Washington DC"})
+        Me.ComboBoxDest.Location = New System.Drawing.Point(206, 68)
+        Me.ComboBoxDest.Name = "ComboBoxDest"
+        Me.ComboBoxDest.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBoxDest.TabIndex = 3
+        '
+        'btnLookUp
+        '
+        Me.btnLookUp.Location = New System.Drawing.Point(29, 168)
+        Me.btnLookUp.Name = "btnLookUp"
+        Me.btnLookUp.Size = New System.Drawing.Size(75, 23)
+        Me.btnLookUp.TabIndex = 4
+        Me.btnLookUp.Text = "Look Up"
+        Me.btnLookUp.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(29, 217)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.AutoSize = True
+        Me.lblAnswer.Location = New System.Drawing.Point(172, 168)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(0, 13)
+        Me.lblAnswer.TabIndex = 6
+        '
+        'lblDistance
+        '
+        Me.lblDistance.AutoSize = True
+        Me.lblDistance.Location = New System.Drawing.Point(172, 144)
+        Me.lblDistance.Name = "lblDistance"
+        Me.lblDistance.Size = New System.Drawing.Size(49, 13)
+        Me.lblDistance.TabIndex = 7
+        Me.lblDistance.Text = "Distance"
+        '
+        'calcForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(389, 277)
+        Me.Controls.Add(Me.lblDistance)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnLookUp)
+        Me.Controls.Add(Me.ComboBoxDest)
+        Me.Controls.Add(Me.ComboBoxDepart)
+        Me.Controls.Add(Me.lblDest)
+        Me.Controls.Add(Me.lblDepart)
+        Me.Name = "calcForm"
+        Me.Text = "Calc Distance"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblDepart As System.Windows.Forms.Label
+    Friend WithEvents lblDest As System.Windows.Forms.Label
+    Friend WithEvents ComboBoxDepart As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBoxDest As System.Windows.Forms.ComboBox
+    Friend WithEvents btnLookUp As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblAnswer As System.Windows.Forms.Label
+    Friend WithEvents lblDistance As System.Windows.Forms.Label
+
+End Class
---
+++ travel_8_5/Calc Distance.vb
@@ -0,0 +1,61 @@
+﻿'Project:       Programming Exercise 8.5
+'Date:          10/14/2012
+'Programmer:    Angelo Maragos
+'Description:   This program calculates distance between select cities 
+
+Public Class calcForm
+
+    ' declare array
+
+    Dim travelInteger(,) As Integer =
+        {{0I, 1004I, 1753I, 2752I, 3017I, 1520I, 1507I, 609I, 3155I, 448I},
+         {1004I, 0I, 921I, 1780I, 2048I, 1397I, 919I, 515I, 2176I, 709I},
+         {1753I, 921I, 0I, 1230I, 1399I, 1343I, 517I, 1435I, 2234I, 1307I},
+         {2752I, 1780I, 1230I, 0I, 272I, 2570I, 1732I, 2251I, 1322I, 2420I},
+         {3017I, 2048I, 1399I, 272I, 0I, 2716I, 1858I, 2523I, 1278I, 2646I},
+         {1520I, 1397I, 1343I, 2570I, 2716I, 0I, 860I, 1494I, 3447I, 1057I},
+         {1507I, 919I, 517I, 1732I, 1858I, 860I, 0I, 1307I, 2734I, 1099I},
+         {609I, 515I, 1435I, 2251I, 2523I, 1494I, 1307I, 0I, 2820I, 571I},
+         {3155I, 2176I, 2234I, 1322I, 1278I, 3447I, 2734I, 2820I, 0I, 2887I},
+         {448I, 709I, 1307I, 2420I, 2646I, 1057I, 1099I, 571I, 2887I, 0I}}
+
+
+    Private Sub btnExit_Click(sender As System.Object, e As System.EventArgs) Handles btnExit.Click
+        Me.Close()
+
+        'exits program
+
+    End Sub
+
+
+
+
+    Private Sub btnLookUp_Click(sender As System.Object, e As System.EventArgs) Handles btnLookUp.Click
+
+        'declare variables
+
+        Dim destIndex As Integer
+        Dim departIndex As Integer
+
+
+        ' set equal to the index value of comboBox items
+        departIndex = ComboBoxDepart.SelectedIndex
+        destIndex = ComboBoxDest.SelectedIndex
+
+
+        'catch exception...not making two selections, one in each drop down
+
+        Try
+
+            lblAnswer.Text = travelInteger(departIndex, destIndex).ToString()
+
+        Catch ex As Exception
+
+
+            MessageBox.Show(" Make a selection in both fields.", "Data Entry Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+
+        End Try
+
+
+    End Sub
+End Class
